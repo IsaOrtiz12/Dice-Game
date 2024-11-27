303e0483c5aca76b2b1020cee7b8c9efb335cf7d
IsaOrtiz12 <ortizisabelle08@gmail.com>
2024-11-26 19:10:53 -0500

    Added bonus die, also started on the actual gameplay

--- #Dice-Game-Script.py
+++ #Dice-Game-Script.py
@@ -32,9 +32,23 @@ def play_turn():
     while True:
         #tuple out?
         if len(set(dice)) == 1:
-            if dice[0] ==6: #DEMON DIE MUHAHAHAHAHAHAHHAHAHAHAHAHAHAAAAAAAAAAA
-                print("You rolled three sixes! DEMON DIE ACTIVATED")
+            if dice[0] ==6: #DEMON DIE 
+                print("You rolled three sixes! DEMON DIE MODE")
                 demon_die = random.randint(1,6)
-                print(d"DEMON DIE rolled: {demon_die}")
+                print(f"DEMON DIE rolled: {demon_die}")
                 return 18 - demon_die
-            print(f"Tuple out! You rolled three ones! Bonus Die Activated")
+            print(f"Tuple out! You rolled three {dice[0]}'s! No points for this round")
+            return 0
+        elif dice.count (1) == 3: #bonus die
+            print("you rolled three ones! BONUS DIE MODE")
+            bonus_die = random.randint(1,6)
+            print(f"Bonus die rolled: {bonus_die}")
+            return 3 * bonus_die
+        
+        fixed_dice = [d for d in dice if dice.count(d) > 1]
+        if fixed_dice:
+            print(f"fixed dice: {fixed_dice}")
+
+        free_dice = len(dice) - len(fixed_dice)
+        if free_dice > 0: 
+            reroll = input("do you want to re-rolla free dice? (yes/no) TYPE CAREFULLY OR PROGRAM WILL IMPLODE").lower()       
